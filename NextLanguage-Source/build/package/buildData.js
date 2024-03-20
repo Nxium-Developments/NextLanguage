b7b4233aa25bc5fdd85194131a1d9847f075764a
RBLAuthorizations <157597683+RBLAuthorizations@users.noreply.github.com>
2024-03-20 18:02:23 +0700

    a

--- NextLanguage-Source/build/package/buildData.js
+++ NextLanguage-Source/build/package/buildData.js
@@ -1,60 +1,48 @@
 // buildData.js
 
-const getCurrentFilenames = require('../../modules/path-ifFileExists')
 const fs = require('fs');
 
 function development_build(check) {
-    
-    if (check) {
-        getCurrentFilenames();
-        console.log("\nDevelopment Release Type:",
-            fs.readFileSync("./release_build.type_xc", "utf8"));
-    } else {
-        return "Invaild use of arguments correct statement use is: development_build(check)";
-    }
+           
+    console.log("\nDevelopment Release Type:"),
+    console.log(
+        fs.readFileSync(__dirname + "\\id\\release_build.type_xc", "utf8")
+    );
 }
 
 function development_version(check) {
-
-    if (check) {
-        getCurrentFilenames();
-        console.log("\nDevelopment Release Version:",
-            fs.readFileSync("./versions/v1.3.core_nxa", "utf8"));
-    } else {
-        return "Invaild use of arguments correct statement use is: development_version(check)";
-    }
+       
+    console.log("\nDevelopment Release Version:"),
+    console.log(
+        fs.readFileSync(__dirname + "\\id\\versions\\v1.4.core_nxa", "utf8")
+    );
 }
 
 function development_type(check) {
-
-    if (check) {
-        getCurrentFilenames();
-        console.log("\nDevelopment Product Type:",
-            fs.readFileSync("./type/computer_language.core_nxa", "utf8"));
-    } else {
-        return "Invaild use of arguments correct statement use is: development_type(check)";
-    }
+       
+    console.log("\nDevelopment Product Type:",
+    fs.readFileSync(__dirname + "\\id\\type\\computer_language.core_nxa", "utf8"));
 }
 
 function development_mode(check, set) {
 
     if (check) {
-        getCurrentFilenames();
+        
         console.log("\nDevelopment Mode: ",
-            fs.readFileSync("./dev/config.dev_nxa", "utf8"));
+            fs.readFileSync(__dirname + "\\id\\dev\\config.dev_nxa", "utf8"));
     } else if (set) {
         if (set == "true") {
-            fs.writeFileSync("./dev/config.dev_nxa" + set, "true");
+            fs.writeFileSync(__dirname + "\\id\\dev\\config.dev_nxa", + set, "true");
 
-            getCurrentFilenames();
+            
             console.log("\nDevelopment Mode: ",
-                fs.readFileSync("./dev/config.dev_nxa", "utf8"));
+                fs.readFileSync(__dirname + "\\id\\dev\\config.dev_nxa", "utf8"));
         } else if (set == "false") {
-            fs.writeFileSync("./dev/config.dev_nxa" + set, "false");
+            fs.writeFileSync(__dirname + "\\id\\dev\\config.dev_nxa", + set, "false");
 
-            getCurrentFilenames();
+            
             console.log("\nDevelopment Mode: ",
-                fs.readFileSync("./dev/config.dev_nxa", "utf8"));
+                fs.readFileSync(__dirname + "\\id\\dev\\config.dev_nxa", "utf8"));
         }
     } else {
         return "Invaild use of arguments correct statement use is: development_mode(check, set)";
