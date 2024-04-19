b867611638f927371a34ec202f5c3a6dfd6cdabd
RBLAuthorizations <102461124+AshleyNex@users.noreply.github.com>
2024-04-19 08:25:17 +0700

    v1.7-dev_1

--- NextLanguage-Source/app.js
+++ NextLanguage-Source/app.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+const date = require('date-and-time');
+
+
+const now = new Date();
+const pattern = date.compile('YYYY-MM-DD');
+const currenttime = date.format(now, pattern);
 
 function executeNxlFile(filePath) {
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -11,30 +17,58 @@ function executeNxlFile(filePath) {
             eval(data);
         } catch (e) {
             console.error('Error executing NXL code:', e);
+        } try {
+            eval(data);
+        } catch (b) {
+            console.error('Error executing NXL code:', b);
         }
     });
+}
+
+function patches_main() {
+    require('./patches/v1.6-patch_vdetection-err.js');
 }
 
 if (!fs.existsSync('../config.nxconf')) {
     const filePath = process.argv[2];
 
-    fs.writeFileSync(filePath, 'console.log(`Hello World`)', 'utf8', (err) => {
+    fs.writeFile('../config.nxconf', filePath, 'utf8', (err) => {
         if (err) {
             console.error(err);
             return;
         }
     })
+
+        
+    if (!filePath) {
+        console.error('Please provide the path to the .nxl file as an argument.');
+        process.exit(1);
+    }
+
+    fs.writeFileSync('../' + filePath, 'console.log(`Hello World`)', 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    })
+
+    const currenttime = date.format(now, pattern); 
 
     fs.copyFileSync('init.js', '../init.js')
 
     fs.copyFileSync('./.README/enable.bat', '../enable.bat')
-
-    fs.mkdirSync('../logs/ready.temp')
-    fs.writeFileSync('../logs/initialize.log', `
+    
+    fs.writeFileSync('./build/lib/log/' + 'NXL_Startup-Initization.log', `
     Visit https://github.com/Nxium-Developments/NextLanguage for
     more information on the configuration of this log file.
 
-    Initialized Successfully`, 'utf8', (err) => {
+    Initialized Successfully` + `
+    
+    < ---------------------------------------------------------------- >
+
+    Log File: ./build/lib/log/${currenttime}-NXL_Startup.log
+
+    `, 'utf8', (err) => {
         if (err) {
             console.error(err);
             return;
@@ -45,47 +79,7 @@ if (!fs.existsSync('../config.nxconf')) {
         const filePath = fs.readFileSync('../config.nxconf', 'utf8');
     
         executeNxlFile(filePath);
-    } else if (!fs.existsSync('../config.nxconf')) {
-        const filePath = process.argv[2];
-    
-        if (!filePath) {
-            console.error('Please provide the path to the .nxl file as an argument.');
-            process.exit(1);
-        } else if (!fs.existsSync('../config.nxconf')) {
-            if (!fs.existsSync('../config.nxconf')) {
-                fs.writeFile('../config.nxconf', filePath, 'utf8', (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-            });
-            }
-        }
-    
-        executeNxlFile(filePath);
-    }
-} else {
-    if (fs.existsSync('../config.nxconf')) {
-        const filePath = fs.readFileSync('../config.nxconf', 'utf8');
-    
-        executeNxlFile(filePath);
-    } else if (!fs.existsSync('../config.nxconf')) {
-        const filePath = process.argv[2];
-    
-        if (!filePath) {
-            console.error('Please provide the path to the .nxl file as an argument.');
-            process.exit(1);
-        } else if (!fs.existsSync('../config.nxconf')) {
-            if (!fs.existsSync('../config.nxconf')) {
-                fs.writeFile('../config.nxconf', filePath, 'utf8', (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-            });
-            }
-        }
-    
-        executeNxlFile(filePath);
     }
 }
+
+patches_main();
