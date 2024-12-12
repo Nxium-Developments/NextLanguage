84faa7e22ae7ce38fd31ee932e1390bed70ddf8e
AshleyNex <contact.niyaarli@gmail.com>
2024-12-12 18:49:52 +0700

    Updated execute function commands to actually be in use!

--- NextLanguage-Source/modules/executeFunction.js
+++ NextLanguage-Source/modules/executeFunction.js
@@ -1,12 +1,29 @@
-const debugOutput = require("./debugOutput");
-const functions = require('../modules/localStorage');
+const setFunction = require('./localStorage.js').setFunction;
+const addOutput = require('./addOutput.js');
 
 // Helper to execute functions
-module.exports = function executeFunction(line, name, param) {
-    if (functions[name]) {
-      const fn = functions[name];
-      if (typeof fn === "function") fn(param);
-    } else {
-      debugOutput(line, `Error: Function '${name}' not found.`);
+module.exports = function executeFunction(lines, inline, name, functionMatch) {
+    // Get the function body
+    const functionBodyIndex = lines.indexOf(inline) + 1;
+
+    // Get the function body contents
+    const functionBody = inline.codePointAt(0) === 91 ? lines.slice(functionBodyIndex).join("\n") : lines[functionBodyIndex];
+
+    // Separates the function blocks.
+    const Block = lines.slice(lines.indexOf(inline) + 1).join('\n').split('\n@end')[0];
+
+    // Sets the actual Function Input
+    functionMatch.input = functionBody
+
+    // Register the function contents
+    setFunction(name, functionMatch)
+    
+    // Checks if the function body has any registered commands
+    if (Block?.startsWith(":output")) {
+      // Separates the contents of @output
+      const text = Block.split(':output').slice(1).join('\n').split('\n:end')[0].trim();
+
+      // Outputs the text
+      addOutput(text);
     }
-  }
+}
