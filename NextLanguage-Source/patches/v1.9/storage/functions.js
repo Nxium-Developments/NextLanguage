b27a681d1ca83f24c596c7d07fd4921b6382f1dd
AshleyNex <contact.niyaarli@gmail.com>
2024-12-14 12:40:28 +0700

    Moved imports logic for compiler.js

---
+++ NextLanguage-Source/patches/v1.9/storage/functions.js
@@ -0,0 +1,13 @@
+const addOutput = require('../../../modules/addOutput');
+const ifHandler = require('./ifHandler.js');
+const debugOutput = require('../../../modules/debugOutput');
+const parseVariable = require('../../../modules/parseVariable');
+const executeFunction = require('../../../modules/executeFunction');
+
+module.exports = {
+    addOutput,
+    ifHandler,
+    debugOutput,
+    parseVariable,
+    executeFunction,
+}
---
+++ NextLanguage-Source/patches/v1.9/storage/storage.js
@@ -0,0 +1,13 @@
+const debugMode = require('../modules/debugmode.js');
+const packageAdvancedCommand = require('../modules/packageadvanced.js');
+const variable = require('../modules/variable.js');
+const outputCommand = require('../modules/output.js');
+const ifCommand = require('../modules/if.js');
+
+module.exports = {
+    debugMode,
+    packageAdvancedCommand,
+    variable,
+    outputCommand,
+    ifCommand
+}
