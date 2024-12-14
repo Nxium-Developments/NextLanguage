c55349f02d573e69c271ced1b59f7f74a003fa8f
AshleyNex <contact.niyaarli@gmail.com>
2024-12-14 12:38:40 +0700

    Moved package-main if statement logics

---
+++ NextLanguage-Source/patches/v1.9/modules/parts/packageMain.js
@@ -0,0 +1,20 @@
+module.exports = function packageMain(line, main, packages) {
+    // Sets the main package
+    if (main === "root/me") {
+        setPackageMain(main); // Sets the main package
+        // Debugging for Developers
+        debugOutput(line, `Main package set to: ${packages.main}`);
+    } else {
+        try {
+            const read = main.split("root/")[0]; // Extracts the read path
+            
+            // Reads the file contents (currently disabled)
+            // const contents = fs.readFileSync(path.join(read), 'utf8');
+
+            addPackageCommand(main); // Adds the command to modules/localStorage.js
+            debugOutput(line, `Command package added: ${main}`); // Debug output
+        } catch (err) {
+            debugOutput(line, `Error reading package: ${err.message}`);
+        }
+    }
+}
