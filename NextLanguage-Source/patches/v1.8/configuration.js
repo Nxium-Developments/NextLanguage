8cc60f261b5bf3798d2312d5e8ee1e7ba27a964e
AshleyNex <contact.niyaarli@gmail.com>
2024-12-14 10:07:16 +0700

    Removed v1.8 unnesscary configurations

--- NextLanguage-Source/patches/v1.8/configuration.js
+++ NextLanguage-Source/patches/v1.8/configuration.js
@@ -1,44 +1,5 @@
-const fs = require('fs');
-const run = require('./rebulid.js');
-const path = require('path');
-
-const contents = require('./contents.js');
-
-if (!fs.existsSync('config.nxconf')) {
-    const filePath = process.argv[2];
-
-    if (!fs.existsSync(filePath)) {
-        fs.writeFileSync(filePath, contents, 'utf8', (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        })
-    }
-
-    if (fs.existsSync('config.nxconf')) {
-        const filePath = fs.readFileSync('config.nxconf', 'utf8');
-    
-        // Execute the .nxl file
-        run(filePath);
-    } else if (!fs.existsSync('config.nxconf')) {
-        const filePath = process.argv[2];
-    
-        if (!filePath) {
-            console.error('Please provide the path to the .nxl file as an argument.');
-            process.exit(1);
-        } else if (!fs.existsSync('config.nxconf')) {
-            if (!fs.existsSync('config.nxconf')) {
-                fs.writeFile('config.nxconf', filePath, 'utf8', (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-            });
-            }
-        }
-    
-        // Execute the .nxl file
-        run(filePath);
-    }
+function config()  {
+    console.log('hu')
 }
+
+config()
