8b577941c21d102512b39af9d577f5ec4c0df4d0
AshleyNex <contact.niyaarli@gmail.com>
2024-12-14 10:08:23 +0700

    updated config initialization

--- NextLanguage-Source/patches/v1.8/initializer.js
+++ NextLanguage-Source/patches/v1.8/initializer.js
@@ -1,20 +1,22 @@
 const run = require('./rebulid.js');
 const fs = require('fs');
 
-if (fs.existsSync('config.nxconf')) {
-    const filePath = fs.readFileSync('config.nxconf', 'utf8');
+const config = require('./contents.js');
+
+if (fs.existsSync(config)) {
+    const filePath = fs.readFileSync(config, 'utf8');
 
     // Execute the .nxl file
     run(filePath);
-} else if (!fs.existsSync('config.nxconf')) {
+} else if (!fs.existsSync(config)) {
     const filePath = process.argv[2];
 
     if (!filePath) {
         console.error('Please provide the path to the .nxl file as an argument.');
         process.exit(1);
-    } else if (!fs.existsSync('config.nxconf')) {
-        if (!fs.existsSync('config.nxconf')) {
-            fs.writeFile('config.nxconf', filePath, 'utf8', (err) => {
+    } else if (!fs.existsSync(config)) {
+        if (!fs.existsSync(config)) {
+            fs.writeFile(config, filePath, 'utf8', (err) => {
             if (err) {
                 console.error(err);
                 return;
