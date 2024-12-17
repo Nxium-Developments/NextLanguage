ecd00476c81914fb3ad7768d8f9cf99113326b8c
AshleyNex <contact.niyaarli@gmail.com>
2024-12-17 18:35:35 +0700

    Completely refractored package-advanced.js as a separate file.

--- NextLanguage-Source/patches/v1.9/modules/packageadvanced.js
+++ NextLanguage-Source/patches/v1.9/modules/packageadvanced.js
@@ -1,24 +1,7 @@
-const createPreload = require("./nodejs/createPreload");
+// Removed packageAdvanced
 
-module.exports = async function packageAdvancedCommand(line, packages) {
-    const script = await createPreload();
+module.exports = async function packageAdvancedCommand(line, packages, setPackageAdvanced, debugOutput) {
+    
 
-    // Handle :package-advanced directive
-    if (line.startsWith(":package-advanced")) {
-        // Creates a Separator for variables given within a space
-        const match = line.match(/:package-advanced (.+);/);
-
-        if (match) {
-            setPackageAdvanced("true"); // Sets an package to advanced mode
-            
-            if (script === true) {
-                debugOutput(line, `Running preload script as default (No commands nested/nor listed)`);
-            }
-
-            debugOutput(line, `Advanced mode: ${packages.advanced}`); // Debug Output
-        } else {
-            setPackageAdvanced("false"); // Sets an package to normal mode
-            debugOutput(line, `Advanced mode: ${packages.advanced}`); // Debug Output
-        }
-    }
+    
 }
