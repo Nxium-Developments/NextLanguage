91cdf9dbfb512a1632c12f088bbddd89b4760e2d
AshleyNex <contact.niyaarli@gmail.com>
2024-12-17 17:33:52 +0700

    Added configurationFile;

--- NextLanguage-Source/patches/v1.9/modules/packageadvanced.js
+++ NextLanguage-Source/patches/v1.9/modules/packageadvanced.js
@@ -1,4 +1,8 @@
-module.exports = function packageAdvancedCommand(line, packages) {
+const createPreload = require("./nodejs/createPreload");
+
+module.exports = async function packageAdvancedCommand(line, packages) {
+    const script = await createPreload();
+
     // Handle :package-advanced directive
     if (line.startsWith(":package-advanced")) {
         // Creates a Separator for variables given within a space
@@ -6,6 +10,11 @@ module.exports = function packageAdvancedCommand(line, packages) {
 
         if (match) {
             setPackageAdvanced("true"); // Sets an package to advanced mode
+            
+            if (script === true) {
+                debugOutput(line, `Running preload script as default (No commands nested/nor listed)`);
+            }
+
             debugOutput(line, `Advanced mode: ${packages.advanced}`); // Debug Output
         } else {
             setPackageAdvanced("false"); // Sets an package to normal mode
