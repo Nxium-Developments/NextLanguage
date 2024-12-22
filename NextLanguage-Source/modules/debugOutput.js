e6e317e1f154a1265ed7f39cdf016755aee27196
AshleyNex <contact.niyaarli@gmail.com>
2024-12-22 14:28:25 +0700

    Refactored Commented code

--- NextLanguage-Source/modules/debugOutput.js
+++ NextLanguage-Source/modules/debugOutput.js
@@ -5,14 +5,7 @@ const debug = getPackages().debugMode;
 // Debug Output Modules
 module.exports = function debugOutput(line, text) {
     // Check if debug mode is enabled
-    // if (debug === false) return;
-
-    // Parse debug message from the line
-    // const debugMatch = line.match(/output:debug\((.+?)\)/);
-    // if (debugMatch) {
-    //     const [, debugText] = debugMatch;
-    //     addOutput(`Debug: ${debugText}`);
-    // }
+    if (debug === false) return;
 
     // Output provided text as debug info
     if (text) {
