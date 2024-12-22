d5a2e7ecf92447b9399fdc4a702f367b626d9dd3
AshleyNex <contact.niyaarli@gmail.com>
2024-12-22 14:30:09 +0700

    Removed unnescary imports

--- NextLanguage-Source/patches/v1.9/storage/ifHandler.js
+++
@@ -1,6 +0,0 @@
-const safeEval = require('../../../modules/safeEval');
-const IfStatementHandler = require('../../../modules/IfStatementHandler');
-const addOutput = require('../../../modules/addOutput');
-const ifHandler = IfStatementHandler(addOutput, safeEval);
-
-module.exports = ifHandler;
