504da6a423593f1beb85057a63fcff8cf079b8fa
AshleyNex <contact.niyaarli@gmail.com>
2024-12-26 14:40:12 +0700

    Added update reminder and allowed BUILD_CONFIG to run

--- NextLanguage-Source/patches/v1.8/configuration.js
+++ NextLanguage-Source/patches/v1.8/configuration.js
@@ -3,22 +3,21 @@ const path = require('path');
 const runConfig = require('./v1.9-v1.8/utils/config_compiler');
 const configurationFile = require('./v1.9-v1.8/config_data');
 
-async function readConfig(file, contents) {
+async function readConfig(file) {
     const response = fs.readFileSync(file, 'utf8');
     const nxlCode = await response;
 
     // Parse and execute the NXL code
     const lines = nxlCode.split("\n").map(line => line.trim());
 
-    runConfig(lines, contents);
+    runConfig(lines);
 }
 
 // TODO: Create a config file. And update config function.
-function config()  {
-    if (fs.existsSync(path)) {
-        const path = path.join(__dirname, '../../../BUILD_CONFIG');
-        const contents = fs.readFileSync(configPath, 'utf-8');
-        readConfig(path, contents);
+async function config()  {
+    if (fs.existsSync(path.join(__dirname, '../../../BUILD_CONFIG'))) {
+        const pathFile = path.join(__dirname, '../../../BUILD_CONFIG');
+        readConfig(pathFile);
     } else {
         fs.writeFileSync(path.join(__dirname, '../../../BUILD_CONFIG'), configurationFile, 'utf-8', (err) => {
             if (err) {
