16aca36d798f9211bb21d42caaa3a9718eeb75d2
cassitly <contact.niyaarli@gmail.com>
2024-12-30 16:26:25 +0700

    Removed debug.js

--- NextLanguage-Source/debugger/debug.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = function debug(line) {
-    // Handle debug command
-    if (line.startsWith("@debug")) {
-        // Refer to modules/debugOutput.js
-        debugOutput(line.split("@debug")[1].trim());
-    }
-}
