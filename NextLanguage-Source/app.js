b4d7afbaa21c7069a2506e0dddd24f124add7a99
cassitly <157597683+RBLAuthorizations@users.noreply.github.com>
2024-12-30 16:36:39 +0700

    Updated imports

--- NextLanguage-Source/app.js
+++ NextLanguage-Source/app.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const run = require('./patches/v1.8/rebulid.js');
+const run = require('./package/start.js');
 
 function patches_main() {
-    require('./patches/v1.8/builder.js')
-    require('./patches/v1.6-patch_vdetection-err.js');
     require('./patches/v1.7-startup.js');
     require('./patches/v1.8/configuration.js');
 }
