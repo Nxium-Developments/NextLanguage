be26116ab7b756c81247d115c7678275b6369159
cassitly <157597683+RBLAuthorizations@users.noreply.github.com>
2024-12-30 16:32:49 +0700

    Added signed code checks

--- NextLanguage-Source/package/bulit-in/Secure/default.js
+++ NextLanguage-Source/package/bulit-in/Secure/default.js
@@ -1,33 +1,43 @@
-module.exports = function Plugin(file, name) {
-    const path = require('path');
-
+module.exports = function Plugin(file, name, signature) {
     const addOutput = require('../../../modules/functions/addOutput.js');
     const debugOutput = require('../../../modules/functions/debugOutput.js');
 
-    const Local = require('../../../modules/class/temp/Local.js');
-    const Package = require('../../../modules/class/Packages.js');
-    
-    const package = new Package;
-
-    const { getVariables, setVariable } = require('../../../modules/functions/temp/Variables.js');
-    const { getFunctions, setFunction } = require('../../../modules/functions/temp/Functions.js');
-
-    const createPreload = require("../../../patches/v1.9/modules/nodejs/pre/createPreload.js");
-    const runPreload = require("../../../patches/v1.9/modules/nodejs/pre/runPreload.js");
-
-    const createPostload = require("../../../patches/v1.9/modules/nodejs/post/createPostload.js");
-    const runPostload = require("../../../patches/v1.9/modules/nodejs/post/runPostload.js");
+    const Information = require(signature);
+    const ExtractInfo = Information()
+    const packname = ExtractInfo.name
+    const packenabled = ExtractInfo.enabled
+    const author = ExtractInfo.author
 
     // This is currently running unsignned code.
     // Fix this by implementing proper signature verification.
     // const isSigned = await verifySignature(path.join(__dirname, file));
-    if (package.name === name || true) {
-        debugOutput(`Checking Plugin Signature: ${name}`);
-        // Remove the true value to run signed code    
-        // package.strings.full.package.user.enabled === true || 
-        if (true) {
+    if (packname === name) {
+        if (packenabled === true) {
             debugOutput(`Loaded Plugin: ${name}`);
-            eval(file)
+            eval(`const path = require('path');
+
+            const addOutput = require('../../../modules/functions/addOutput.js');
+            const debugOutput = require('../../../modules/functions/debugOutput.js');
+        
+            const Local = require('../../../modules/class/temp/Local.js');
+            const Package = require('../../../modules/class/Packages.js');
+            
+            const package = new Package;
+            const local = new Local;
+        
+            const { getVariables, setVariable } = require('../../../modules/functions/temp/Variables.js');
+            const { getFunctions, setFunction } = require('../../../modules/functions/temp/Functions.js');
+        
+            const createPreload = require("../../../patches/v1.9/modules/nodejs/pre/createPreload.js");
+            const runPreload = require("../../../patches/v1.9/modules/nodejs/pre/runPreload.js");
+        
+            const createPostload = require("../../../patches/v1.9/modules/nodejs/post/createPostload.js");
+            const runPostload = require("../../../patches/v1.9/modules/nodejs/post/runPostload.js");
+            ${file}
+            Service()`); // Load the plugin (file)
+        } else {
+            addOutput(`Failed to load Plugin: ${name}`);
+            addOutput(`Reason: Plugin is disabled by: ${author}`);
         }
     } else {
         addOutput(`Failed to load Plugin: ${name}`);
