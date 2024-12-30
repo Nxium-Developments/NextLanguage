84de4bb6b1a9faaefedcf4e26518e45cabe4aac4
cassitly <157597683+RBLAuthorizations@users.noreply.github.com>
2024-12-30 16:35:31 +0700

    Updated new imports

--- NextLanguage-Source/modules/functions/debugOutput.js
+++ NextLanguage-Source/modules/functions/debugOutput.js
@@ -1,6 +1,6 @@
 const addOutput = require('./addOutput.js');
-const Debug = require('../class/debug/Debug.js');
-const debug = new Debug();
+const Debug = require('../class/Debug.js');
+const debug = new Debug().debugMode;
 
 // Debug Output Modules
 module.exports = function debugOutput(text) {
