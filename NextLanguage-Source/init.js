933ab7301c80d81ec3e54508f144ff6af2a8eb17
cassitly <157597683+RBLAuthorizations@users.noreply.github.com>
2024-12-30 16:36:47 +0700

    removed a import

--- NextLanguage-Source/init.js
+++ NextLanguage-Source/init.js
@@ -1,7 +1,6 @@
 function patches_main() {
     require('./patches/v1.8/configuration.js');
     require('./patches/v1.8/initializer.js');
-    require('./patches/v1.8/logicalizer.js');
 }
 
 patches_main();
