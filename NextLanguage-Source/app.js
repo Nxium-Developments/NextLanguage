03ee4426c7525fb337940ff6d2ac9b77e88097f3
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-19 18:20:08 +0700

    Updated incorrect values

--- NextLanguage-Source/app.js
+++ NextLanguage-Source/app.js
@@ -5,18 +5,10 @@ const enableDevelopment = require("./package/bulit-in/Debugger/default.js");
 
 // new enableDevelopment("function-state")
 
-const run = require('./package/start.js');
-const config = require('./patches/v1.8/returns.js').returns().path.config;
-
 async function patches_main() {
     require('./patches/initials.js');
     require('./patches/v1.8/configuration.js');
     require('./patches/v1.8/returns.js');
-
-    if (fs.existsSync(config)) {
-        const filePath = fs.readFileSync(config, 'utf8');
-        run(filePath);
-    }
 }
 
 patches_main();
