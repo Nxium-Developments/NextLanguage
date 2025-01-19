9c7dce5aca6cce73e5c7c7ca35af3b61fed463b2
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-19 19:47:25 +0700

    Removed unnesscary dependencies

--- NextLanguage-Source/app.js
+++ NextLanguage-Source/app.js
@@ -1,10 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-const enableDevelopment = require("./package/bulit-in/Debugger/default.js");
-
-// new enableDevelopment("function-state")
-
 async function patches_main() {
     require('./patches/initials.js');
     require('./patches/v1.8/configuration.js');
