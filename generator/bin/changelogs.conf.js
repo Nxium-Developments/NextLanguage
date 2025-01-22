0259d0afbdacec7afac98650078c7d1c37dd9c99
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-22 08:16:32 +0700

    Update changelogs.conf.js

--- generator/bin/changelogs.conf.js
+++ generator/bin/changelogs.conf.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = function changelogs() {
   return {
-    version: readFileSync(path.join(__dirname, "../../NextLanguauge-Source/package/bulit-in/versions.package")).split("\n")
+    version: readFileSync(path.join(__dirname, "../../NextLanguauge-Source/package/bulit-in/versions.package"), "utf8").split("\nver: Build_")[1],
+    changes: readFileSync(path.join(__dirname, "../../CHANGELOG.md"), "utf8")
   }
 }
