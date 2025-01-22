01bee08a0b9105d2d2e008a8bf5b994aecd96e4f
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-22 09:16:21 +0700

    Update nextlang-cmd.cpp

--- generator/bin/scripts/nextlang-cmd.cpp
+++ generator/bin/scripts/nextlang-cmd.cpp
@@ -10,21 +10,31 @@ int main(int argc, char* argv[]) {
 
     // Get the argument
     std::string command = argv[1];
+    std::string file = argv[2];
 
     // Check if the argument matches the condition
     if (command == "build") {
-        std::cout << "Running 'npm run build'..." << std::endl;
+        std::cout << "Building Project..." << std::endl;
 
         // Execute the npm run build command
         int result = std::system("npm run build");
 
         // Check if the command was successful
         if (result == 0) {
-            std::cout << "'npm run build' completed successfully." << std::endl;
+            std::cout << "Completed build successfully." << std::endl;
         } else {
-            std::cerr << "Error: 'npm run build' failed with code " << result << "." << std::endl;
+            std::cerr << "Error: Project build failed with code " << result << "." << std::endl;
             return result;
         }
+    } else if (command == ".") {
+        // Execute the npm run build command
+        std::system("npm start");
+    } else if (command == "run") {
+        if (!file) {
+            std::cerr << "Please provide a file" << std::endl;
+            return 1;
+        }
+        std::system("npm start" << file)
     } else {
         std::cerr << "Error: Invalid command. Use 'build' to run 'npm run build'." << std::endl;
         return 1;
