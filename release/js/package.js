c48069dd5e8418515e3fd148fdabd0585ef8ccb3
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-06-08 10:58:19 +0000

    Update package

--- release/js/package.js
+++ release/js/package.js
@@ -41,13 +41,12 @@ function stopRunningBinary(tempName) {
 }
 
 function applyUpdate(latestDir) {
-  const buildJsonPath = path.join(updateRoot, latestDir, 'build.json');
-  if (!fs.existsSync(buildJsonPath)) {
-    console.error(`❌ build.json not found at ${buildJsonPath}`);
+  if (!fs.existsSync(configPath)) {
+    console.error(`❌ config.json not found at ${configPath}`);
     process.exit(1);
   }
 
-  const buildData = JSON.parse(fs.readFileSync(buildJsonPath, 'utf8'));
+  const buildData = JSON.parse(fs.readFileSync(configPath, 'utf8'));
   const binaries = buildData.binaries || [];
   const memoryBinaries = buildData.binary_data || {}; // optional: { "binaryName": base64String }
 
