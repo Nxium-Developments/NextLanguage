9b4c0bd8b766ad17165170ace4bb502851e1d1e7
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-06-09 22:13:59 +0700

    Multiple changes - New get_executable_oath cmd - Added make_path - Added finalize_install   - Further installs nodejs, python, & gcc - Added new commands to print_help() - Added debug command list filter to print_help() - Added -h & --help commands to use print_help() - Added convert command, to convert images (.png) to source .img - Added extra compile command (very unnesscary.) - Added secret little --finalize-install & -fi - Added new debug command   - Added --getcwd (current working directory) & getcwd to said debug command. - Added convert compiler type.

--- src/nextlang.c
+++ src/nextlang.c
@@ -6,6 +6,101 @@
 #include "./utils/updater.h"
 #include "./utils/package.h"
 #include "./img/imagery.h"
+#include "./utils/download.h"
+
+char base_dir[1024];
+
+// Get directory of current executable
+void get_executable_path() {
+#ifdef _WIN32
+    char path[MAX_PATH];
+    GetModuleFileNameA(NULL, path, MAX_PATH);
+    char *last_slash = strrchr(path, '\\');
+    if (last_slash) *last_slash = '\0';
+    strcpy(base_dir, path);
+#else
+    char path[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", path, PATH_MAX);
+    if (count != -1) {
+        path[count] = '\0';
+        char *last_slash = strrchr(path, '/');
+        if (last_slash) *last_slash = '\0';
+        strcpy(base_dir, path);
+    }
+#endif
+}
+
+char *make_path(const char *filename) {
+    static char full_path[2048];
+#ifdef _WIN32
+    snprintf(full_path, sizeof(full_path), "%s\\%s", base_dir, filename);
+#else
+    snprintf(full_path, sizeof(full_path), "%s/%s", base_dir, filename);
+#endif
+    return full_path;
+}
+
+void finalize_install(int slient) {
+    #define DOWNLOAD_DIR make_path("tmp");
+    #define EXTRACT_DIR make_path("bin");
+    #define ZIP_FILE_PATH make_path("bin/node-v22.16.0-win-x64.zip");
+
+    _mkdir(DOWNLOAD_DIR);
+    _mkdir(EXTRACT_DIR);
+
+    const char *url = "https://nodejs.org/dist/v22.16.0/node-v22.16.0-win-x64.zip";
+    const char *final_name = "nodejs";
+
+    if (!slient) printf("Downloading nodejs...\n");
+    if (download_file(url, ZIP_FILE_PATH)) {
+        fprintf(stderr, "Failed to download file.\n");
+        return 1;
+    }
+
+    if (!slient) printf("Extracting nodejs...\n");
+    if (extract_zip(ZIP_FILE_PATH, EXTRACT_DIR)) {
+        fprintf(stderr, "Failed to extract ZIP.\n");
+        return 1;
+    }
+
+    if (!slient) printf("Renaming nodejs from node-v22.16.0-win-x64 to nodejs...\n");
+    if (rename_extracted_folder(EXTRACT_DIR, final_name)) {
+        fprintf(stderr, "Failed to rename folder.\n");
+        return 1;
+    }
+
+    if (!slient) printf("Cleaning up...\n");
+    remove(ZIP_FILE_PATH);
+    free(ZIP_FILE_PATH);
+
+    if (!slient) printf("Completed nodejs installation.\n");
+    #define NODEJS_PATH make_path("bin/nodejs/node.exe")
+
+    int install_gcc = run_cmd("%s %s --repository=git://gcc.gnu.org/git/gcc.git --destination=%s --params={}",
+        NODEJS_PATH, make_path("packages/libs/git-pull.js"),
+        make_path("packages/gcc")
+    );
+    if (install_gcc != 0) {
+        fprintf(stderr, "Failed to install GCC.\n");
+        return 1;
+    }
+
+    if (!slient) printf("Completed GCC installation.\n");
+
+    int install_python = run_cmd("%s %s --url=%s --output-dir=%s --final-name=python", NODEJS_PATH, make_path("packages/libs/fetch.js"),
+        "https://www.python.org/ftp/python/3.13.4/Python-3.13.4.tgz",
+        EXTRACT_DIR
+    );
+
+    if (install_python != 0) {
+        fprintf(stderr, "Failed to install Python.\n");
+        return 1;
+    }
+
+    if (!slient) printf("Completed Python installation.\n");
+
+    return 0;
+}
 
 void print_help() {
     BuildInfo info;
@@ -16,8 +111,20 @@ void print_help() {
         printf("NextLanguage Compiler (null)\n");
     }
 
-    printf("Usage: nextlang [options] <source.extn|source.img>\n");
-    printf("Options:\n");
+    printf("Usage: nextlang [options/commands] <source.extn|source.img>\n");
+    printf("\nCommands:\n");
+    printf("  convert <file>       Convert an image to a source file\n");
+    printf("  compile <file>       Compile a source file\n");
+
+    printf("\nPackage Commands (UNIMPLEMENTED):\n");
+    printf("  install <package>    Install a package\n");
+    printf("  uninstall <package>  Uninstall a package\n");
+    printf("  update               Update NextLanguage\n");
+    printf("  list                 List installed packages\n");
+    printf("\n\n");
+
+    printf("Options (short: -<option>, long: --<option>):\n");
+    printf("  -h, --help       Show this help message\n");
     printf("  -o <file>        Output filename (default: output.exe)\n");
     printf("  --output <file>  Same as -o\n");
     printf("  --cc <compiler>  Compiler to use (default: gcc)\n");
@@ -27,9 +134,25 @@ void print_help() {
     printf("  --check-updates  Checks for updates\n");
     printf("  --apply-updates  Apply updates\n");
     printf("  --skip-compile   Skip compilation\n");
+
+    if (read_build_info(&info)) {
+        if (info.release_type != "dev") {
+            printf("\n\n");
+            printf("NextLanguage Compiler %s\n", info.build_version);
+            printf("Copyright (c) 2023 NextLanguage Contributors\n");
+            printf("MIT License\n");
+            printf("\n");
+        } else {
+            printf("\n\n");
+            printf("NextLanguage Debug Commands:\n");
+            printf("  --debug <command>, debug <command>  Run a debug command\n");
+            printf("\n");
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
+    get_executable_path();
     init_base_dir();
     set_exe_dir();
 
@@ -49,7 +172,19 @@ int main(int argc, char* argv[]) {
     int skip_compilation = 0;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "--ver") == 0) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            print_help();
+            return 0;
+        }
+
+        // Converts an normal image into a source .img file
+        else if (strcmp(argv[i], "convert") == 0 && i + 1 < argc) {
+            infile = argv[++i];
+            compile_type = "convert";
+        }
+
+
+        else if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "--ver") == 0) {
             BuildInfo info;
             if (read_build_info(&info)) print_build_info(&info);
             return 0;
@@ -61,6 +196,11 @@ int main(int argc, char* argv[]) {
             cflags = argv[++i];
         } else if (strcmp(argv[i], "--silent") == 0) {
             silent = 1;
+        }
+
+        if (strcmp(argv[i], "compile") == 0 && i + 1 < argc) {
+            infile = argv[++i];
+            compile_type = strstr(argv[++i], ".extn") ? "extn" : "img";
         }
         
         // Files to compile
@@ -90,8 +230,14 @@ int main(int argc, char* argv[]) {
             pull_updates = 1;
         }
 
+        // Secret little finalize install feature
+        else if (strcmp(argv[i], "-fi") == 0 || strcmp(argv[i], "--finalize-install") == 0) {
+            finalize_install();
+            return 0;
+        }
+
         // Debugging dev features
-        else if (strcmp(argv[i], "--debug") == 0) {
+        else if (strcmp(argv[i], "--debug") == 0 || strcmp(argv[i], "debug") == 0) {
             BuildInfo info; // Checks if config.json exists
             if (read_build_info(&info)) { // And if it's a dev build
                 if (strcmp(info.release_type, "dev") != 0) {
@@ -107,8 +253,9 @@ int main(int argc, char* argv[]) {
 
             // Debug commands
             char* args = argv[i + 1];
-            if (strstr(args, "test-update") != NULL) {
-                check_for_updates();
+            if (strstr(args, "--getcwd") != NULL || strstr(args, "getcwd") != NULL) {
+                printf("%s\n", getcwd(NULL, 0));
+                return 0;
             }
             
             else {
@@ -185,6 +332,15 @@ int main(int argc, char* argv[]) {
         }
         write_image_png(img, outfile);
         free_image(img);
+        return 0;
+    }
+
+    else if (compile_type == "convert") {
+        int status_convert = run_cmd("python %s --image=\"%s\" --output=\"%s\"", make_path("img/converter.py"), infile, outfile);
+        if (status_convert != 0) {
+            if (!silent) printf("❌ Error converting image\n");
+            return 1;
+        }
         return 0;
     }
 
